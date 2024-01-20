276bc7c78f7535ac9ec7845095dcb2e49fc86b21
Sayed2210 <eh600039@gmail.com>
2024-01-20 13:46:09 +0200

    some updates

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quod tempora
+      fuga possimus cumque doloremque deserunt et a, sint soluta suscipit quos
+      velit voluptate assumenda, hic, eaque reprehenderit beatae voluptatem!
+    </p>
   </div>
 </template>
